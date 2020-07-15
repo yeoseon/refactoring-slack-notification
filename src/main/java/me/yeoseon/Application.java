0557697e3ea0d6c13dbf1eb050cf616156f84ded
yeoseon <devyyskr@gmail.com>
2020-07-15 09:16:51 +0900

    setting(springboot): springboot 설정 (미완)

---
+++ src/main/java/me/yeoseon/Application.java
@@ -0,0 +1,8 @@
+package me.yeoseon;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Applicatoin.class, args);
+    }
+}
---
+++ src/test/java/me/yeoseon/ApplicationTest.java
@@ -0,0 +1,5 @@
+package me.yeoseon;
+
+public class ApplicationTest {
+
+}
