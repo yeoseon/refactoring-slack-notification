a719edfc07b99a1fd9abaf48dcf92de30e9b6537
yeoseon <devyyskr@gmail.com>
2020-07-17 08:49:19 +0900

    test(SlackNotifierTest): 슬랙 메시지 전송 테스트

--- src/main/java/me/yeoseon/dj/domain/SlackNotifier.java
+++ src/main/java/me/yeoseon/dj/domain/SlackNotifier.java
@@ -1,5 +1,27 @@
 package me.yeoseon.dj.domain;
 
-public class SlackNotifier {
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
+
+public class SlackNotifier {
+    private SlackEnvironment slackEnvironment;
+
+    public SlackNotifier() {
+    }
+
+    public SlackNotifier(SlackEnvironment slackEnvironment) {
+        this.slackEnvironment = slackEnvironment;
+    }
+
+    public void notifyMessage(String message) {
+//        String targetUrl = slackEnvironment.getTargetUrl();
+        String targetUrl = "https://hooks.slack.com/services/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";
+        String request = "{\n\"text\": \"안녕\"\n}";
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        restTemplate.postForEntity(targetUrl, message, String.class);
+    }
 }
--- src/test/java/me/yeoseon/domain/SlackNotifierTest.java
+++ src/test/java/me/yeoseon/domain/SlackNotifierTest.java
@@ -1,5 +1,6 @@
 package me.yeoseon.domain;
 
+import me.yeoseon.dj.domain.SlackNotifier;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -12,11 +13,9 @@ public class SlackNotifierTest {
     @Test
     @DisplayName("REST API를 이용하여 slack에 메시지를 Notify 한다.")
     void notifyMessage() {
-        String targetUrl = "https://hooks.slack.com/services/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";
-        String request = "{\n\"text\": \"안녕\"\n}";
+        String message = "안녕";
 
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
-        restTemplate.postForEntity(targetUrl, request, String.class);
+        SlackNotifier slackNotifier = new SlackNotifier();
+        slackNotifier.notifyMessage(message);
     }
 }
