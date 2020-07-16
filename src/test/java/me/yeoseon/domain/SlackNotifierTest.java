1492c7f71ce33b01fc92752699f348d29432898d
yeoseon <devyyskr@gmail.com>
2020-07-17 08:43:11 +0900

    test(nofityMessage): 테스트 코드 통과

--- src/test/java/me/yeoseon/domain/SlackNotifierTest.java
+++ src/test/java/me/yeoseon/domain/SlackNotifierTest.java
@@ -10,12 +10,10 @@ import java.nio.charset.Charset;
 public class SlackNotifierTest {
 
     @Test
-    @DisplayName("REST API를 이용하여 lack에 메시지를 Nofity 한다.")
+    @DisplayName("REST API를 이용하여 slack에 메시지를 Notify 한다.")
     void notifyMessage() {
-        String targetUrl = "https://hooks.slack.com/services/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1";
-        String request = "{\n" +
-                "    \"text\": \"Hello, world.\"\n" +
-                "}";
+        String targetUrl = "https://hooks.slack.com/services/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";
+        String request = "{\n\"text\": \"안녕\"\n}";
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
