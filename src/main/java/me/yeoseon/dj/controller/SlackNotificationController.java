744518588e233d37c23e7be838531be71175ea05
yeoseon <devyyskr@gmail.com>
2020-07-21 08:07:59 +0900

    feat(SlackNotificationController): Controller 생성

---
+++ src/main/java/me/yeoseon/dj/controller/SlackNotificationController.java
@@ -0,0 +1,17 @@
+package me.yeoseon.dj.controller;
+
+import me.yeoseon.dj.domain.SlackEnvironment;
+import me.yeoseon.dj.domain.SlackNotifier;
+
+public class SlackNotificationController {
+
+    private static final String TARGET_URL = "https://hooks.slack.com/services/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";
+
+    public static void play() {
+        SlackEnvironment slackEnvironment = new SlackEnvironment(TARGET_URL);
+
+        SlackNotifier slackNotifier = new SlackNotifier(slackEnvironment);
+
+        slackNotifier.notifyMessage("안뇽안뇽");
+    }
+}
--- src/main/java/me/yeoseon/dj/domain/SlackEnvironment.java
+++ src/main/java/me/yeoseon/dj/domain/SlackEnvironment.java
@@ -1,11 +1,10 @@
 package me.yeoseon.dj.domain;
 
 public class SlackEnvironment {
-    private static final String TARGET_URL = "https://hooks.slack.com/services/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";
     private String targetUrl;
 
-    public SlackEnvironment() {
-        this.targetUrl = TARGET_URL;
+    public SlackEnvironment(String targetUrl) {
+        this.targetUrl = targetUrl;
     }
 
     public String getTargetUrl() {
