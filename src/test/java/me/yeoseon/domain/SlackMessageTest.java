0d8079854414906f6bb4854bc0160753ec61498a
yeoseon <devyyskr@gmail.com>
2020-07-22 09:28:35 +0900

    refactor(SlackMessageTest): 테스트코드 빌드 오류 수정

--- src/test/java/me/yeoseon/domain/SlackMessageTest.java
+++ src/test/java/me/yeoseon/domain/SlackMessageTest.java
@@ -1,5 +1,6 @@
 package me.yeoseon.domain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import me.yeoseon.dj.domain.SlackMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ public class SlackMessageTest {
 
     @Test
     @DisplayName("SlackMessage 내의 정보를 통해 JSON으로 변환한 String을 반환한다.")
-    void convertSlackMessageToJson() {
+    void convertSlackMessageToJson() throws JsonProcessingException {
         SlackMessage message = new SlackMessage();
 
         String expectedString = "";
