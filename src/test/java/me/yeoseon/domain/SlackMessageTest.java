ea483c88a90f6b8db5dce5362d10de11594913b2
yeoseon <devyyskr@gmail.com>
2020-07-22 09:24:28 +0900

    test(SlackMessageTest): 슬랙 메시지 Json 반환 테스트 메소드 추가

--- src/test/java/me/yeoseon/domain/SlackMessageTest.java
+++ src/test/java/me/yeoseon/domain/SlackMessageTest.java
@@ -4,11 +4,16 @@ import me.yeoseon.dj.domain.SlackMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SlackMessageTest {
 
     @Test
     @DisplayName("SlackMessage 내의 정보를 통해 JSON으로 변환한 String을 반환한다.")
     void convertSlackMessageToJson() {
         SlackMessage message = new SlackMessage();
+
+        String expectedString = "";
+        assertThat(message.convertSlackMessageToJson()).isEqualTo(expectedString);
     }
 }
